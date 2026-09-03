107ab74b1eb71d854ec730eb578653d37ce668e7
Sakxam Xeetri <157821692+sakxam-xeetri@users.noreply.github.com>
2026-09-03 16:25:22 +0545

    feat: initialize project structure and configure PlatformIO for ESP32-S3

--- .pio/build/esp32dev/FrameworkArduino/FirmwareMSC.cpp.d
+++
@@ -1,4 +0,0 @@
-.pio/build/esp32dev/FrameworkArduino/FirmwareMSC.cpp.o: \
- C:/Users/ACER/.platformio/packages/framework-arduinoespressif32/cores/esp32/FirmwareMSC.cpp \
- C:/Users/ACER/.platformio/packages/framework-arduinoespressif32/cores/esp32/FirmwareMSC.h \
- C:/Users/ACER/.platformio/packages/framework-arduinoespressif32/cores/esp32/USBMSC.h
---
+++ .pio/build/esp32s3/FrameworkArduino/stdlib_noniso.c.d
@@ -0,0 +1,3 @@
+.pio/build/esp32s3/FrameworkArduino/stdlib_noniso.c.o: \
+ C:/Users/ACER/.platformio/packages/framework-arduinoespressif32/cores/esp32/stdlib_noniso.c \
+ C:/Users/ACER/.platformio/packages/framework-arduinoespressif32/cores/esp32/stdlib_noniso.h
